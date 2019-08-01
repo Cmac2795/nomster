04809ae00c686d2a0c6f125ec247c65be4108cee
Charlie McKinley <mcmckinley@gmail.com>
2019-08-01 04:14:41 +0000

    added dropdown menu for ratings instead of text field

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RAITINGS = {
+    'one star': '1_star',
+    'two star': '2_star',
+    'three star': '3_star',
+    'four star': '4_star',
+    'five star': '5_star'
+  }
+
+  def humanized_rating
+    RAITINGS.invert[self.rating]
+  end
 end
