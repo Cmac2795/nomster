563933eb8f702a65ab2faf2c18067ae449664643
Charlie McKinley <mcmckinley@gmail.com>
2019-08-08 21:51:50 +0000

    lesson 39 add image uploader

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,11 +1,12 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :photos
   
   geocoded_by :address
   after_validation :geocode
 
-  validates :name, length: { minimum: 3 }, presence: true
-  validates :address, presence: true
-  validates :description, presence: true
+  validates :name, presence: true, length: { minimum: 3, 
+    too_short: "must be a minimum of %{count} characters" }
+  validates :address, :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_185611) do
+ActiveRecord::Schema.define(version: 2019_08_08_173416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2019_07_31_185611) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_comments_on_place_id"
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -50,4 +60,5 @@ ActiveRecord::Schema.define(version: 2019_07_31_185611) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "users"
 end
